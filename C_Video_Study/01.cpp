a3911263c8cb0db805a626265462ee8aebc0184e
Sgt-Foley <Experimental_Chemistry@outlook.com>
2023-11-16 15:43:03 +0800

    初识C++

---
+++ C_Video_Study/01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() // 第一个C++程序
+{
+    int a = 10;
+    cout << "hello world" << endl;
+    return 0;
+} /*多行注释*/
---
+++ C_Video_Study/03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()//关键字
+{
+    // int 123a=1;
+    // int 123=1;
+    int a31 = 1;
+    int _123a = 1;
+    int sum = a31 + _123a;
+    cout << sum << endl;
+
+    return 0;
+}
